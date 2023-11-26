4e990b30309b3f6b21544d2fd69faf0bc5dcb135
Zee3469 <152073057+Zee3469@users.noreply.github.com>
2023-11-26 18:00:38 +0500

    Add files via upload

---
+++ App.py
@@ -0,0 +1,162 @@
+import streamlit as st
+from PIL import Image
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+from newspaper import Article
+from deep_translator import GoogleTranslator
+
+
+import io
+import nltk
+nltk.download('punkt')
+
+st.set_page_config(page_title='News Summarization📰', page_icon='./Meta/newspaper.ico')
+
+
+def fetch_news_search_topic(topic):
+    site = 'https://news.google.com/rss/search?q={}'.format(topic)
+    op = urlopen(site)  # Open that site
+    rd = op.read()  # read data from site
+    op.close()  # close the object
+    sp_page = soup(rd, 'xml')  # scrapping data from site
+    news_list = sp_page.find_all('item')  # finding news
+    return news_list
+
+
+def fetch_top_news():
+    site = 'https://news.google.com/news/rss'
+    op = urlopen(site)  # Open that site
+    rd = op.read()  # read data from site
+    op.close()  # close the object
+    sp_page = soup(rd, 'xml')  # scrapping data from site
+    news_list = sp_page.find_all('item')  # finding news
+    return news_list
+
+
+def fetch_category_news(topic):
+    site = 'https://news.google.com/news/rss/headlines/section/topic/{}'.format(topic)
+    op = urlopen(site)  # Open that site
+    rd = op.read()  # read data from site
+    op.close()  # close the object
+    sp_page = soup(rd, 'xml')  # scrapping data from site
+    news_list = sp_page.find_all('item')  # finding news
+    return news_list
+
+
+def fetch_news_poster(poster_link):
+    try:
+        u = urlopen(poster_link)
+        raw_data = u.read()
+        image = Image.open(io.BytesIO(raw_data))
+        st.image(image, use_column_width=True)
+    except:
+        image = Image.open('./Meta/no_image.jpg')
+        st.image(image, use_column_width=True)
+
+
+def translate(summary, language):
+    translated_text = GoogleTranslator(source="auto", target=language).translate(summary)
+    return translated_text
+
+def display_news(list_of_news, news_quantity):
+    c = 0
+    for news in list_of_news:
+        c += 1
+        # st.markdown(f"({c})[ {news.title.text}]({news.link.text})")
+        st.write('**({}) {}**'.format(c, news.title.text))
+        news_data = Article(news.link.text)
+        try:
+            news_data.download()
+            news_data.parse()
+            news_data.nlp()
+
+            summary = news_data.summary
+        except Exception as e:
+            st.error(e)
+        fetch_news_poster(news_data.top_image)
+
+        if 'translation_language' not in st.session_state:
+            st.session_state.translation_language = 'Original'
+        
+        category = ['--Select--', 'Original Text', 'Urdu', 'Arabic', 'Russian']
+        st.session_state.translation_language = st.selectbox('Select your language', category, 
+            key=f"translation_language_{news.title.text}")
+
+
+        with st.expander(news.title.text):
+            if st.session_state.translation_language == 'Original Text':
+                pass
+            if st.session_state.translation_language == 'Urdu':
+                summary = translate(summary, 'ur')
+            elif st.session_state.translation_language == 'Arabic':
+                summary = translate(summary, 'ar')
+            elif st.session_state.translation_language == 'Russian':
+                summary = translate(summary, 'ru')
+            
+            st.markdown(
+                '''<h6 style='text-align: justify;'>{}"</h6>'''.format(summary),
+                unsafe_allow_html=True)
+
+            
+            st.markdown("[Read more at {}...]({})".format(news.source.text, news.link.text))
+        st.success("Published Date: " + news.pubDate.text)
+        if c >= news_quantity:
+            break
+
+
+def run():
+    st.markdown("<h1 style='text-align: center'>News Summarization</h1>", unsafe_allow_html=True)
+    image = Image.open('./Meta/newspaper.png')
+
+    col1, col2, col3 = st.columns([3, 3, 3])
+
+    with col1:
+        st.write("")
+
+    with col2:
+        st.image(image, use_column_width=True)
+
+    with col3:
+        st.write("")
+    category = ['--Select--', 'Trending🔥 News', 'Favourite💙 Topics', 'Search🔍 Topic']
+    cat_op = st.selectbox('Select your Category', category)
+    if cat_op == category[0]:
+        st.warning('Please select Type!!')
+    elif cat_op == category[1]:
+        st.subheader("✅ Here is the Trending🔥 news for you")
+        no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
+        news_list = fetch_top_news()
+        display_news(news_list, no_of_news)
+    elif cat_op == category[2]:
+        av_topics = ['Choose Topic', 'WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS', 'SCIENCE',
+                     'HEALTH']
+        st.subheader("Choose your favourite Topic")
+        chosen_topic = st.selectbox("Choose your favourite Topic", av_topics)
+        if chosen_topic == av_topics[0]:
+            st.warning("Please Choose the Topic")
+        else:
+            no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
+            news_list = fetch_category_news(chosen_topic)
+            if news_list:
+                st.subheader("✅ Here are the some {} News for you".format(chosen_topic))
+                display_news(news_list, no_of_news)
+            else:
+                st.error("No News found for {}".format(chosen_topic))
+
+    elif cat_op == category[3]:
+        user_topic = st.text_input("Enter your Topic🔍")
+        no_of_news = st.slider('Number of News:', min_value=5, max_value=15, step=1)
+
+        if st.button("Search") and user_topic != '':
+            user_topic_pr = user_topic.replace(' ', '')
+            news_list = fetch_news_search_topic(topic=user_topic_pr)
+            if news_list:
+                st.subheader("✅ Here are the some {} News for you".format(user_topic.capitalize()))
+                display_news(news_list, no_of_news)
+            else:
+                st.error("No News found for {}".format(user_topic))
+        else:
+            st.warning("Please write Topic Name to Search🔍")
+
+
+run()
